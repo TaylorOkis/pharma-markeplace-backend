bac1e5c50b1cfbbcbe5de138008c0c570ac76e24
okistaylor <72969270+TaylorOkis@users.noreply.github.com>
2025-08-25 22:13:08 +0100

    Add new folder structure and core configs

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import notFound from "./core/middlewares/notFound.middleware";
+import errorMiddleware from "./core/middlewares/error.middleware";
 
 const app = express();
 
@@ -9,5 +11,8 @@ app.use(cors());
 app.get("/health", (req: Request, res: Response) => {
   res.send("Hello, Welcome! This API is running perfectly.");
 });
+
+app.use(notFound);
+app.use(errorMiddleware);
 
 export default app;
